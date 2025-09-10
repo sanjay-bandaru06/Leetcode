0c908889310decbebc4f6517feef15100c9c8465
Sanjay Manikanta Bandaru <166979529+sanjay-bandaru06@users.noreply.github.com>
2025-09-10 18:36:54 +0530

    Time: 48 ms (37.24%) | Memory: 17.6 MB (89.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,13 @@
 class Solution:
+    def __init__(self):
+        self.dp = {}
     def fib(self, n: int) -> int:
         if n == 0 or n == 1:
             return n
-        
-        ans = self.fib(n-1) + self.fib(n-2)
-        return ans
+
+        if n in self.dp:
+            return self.dp[n]
+
+        self.dp[n] = self.fib(n-1) + self.fib(n-2)
+        return self.dp[n]
         
